a7ae1d16d4272afb84cca4c0abaf9a0d9cb740c4
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-13 21:44:16 +0000

    Update 103-python.c file

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -46,23 +46,20 @@ void print_python_bytes(PyObject *p)
 void print_python_list(PyObject *p)
 {
 	long int size, i;
+	PyListObject *list;
 	PyObject *item;
 
-	if (!PyList_Check(p))
-	{
-		printf("  [ERROR] Invalid List Object\n");
-		return;
-	}
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
 
-	size = PyList_Size(p);
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-
+		item = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((item)->ob_type)->tp_name);
 		if (PyBytes_Check(item))
 			print_python_bytes(item);
 	}
