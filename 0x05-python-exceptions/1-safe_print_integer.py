0a57147b1bec739f4c7939ff058e63c2b4ba6c0a
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-25 15:49:11 +0000

    Update 1-safe_print_integer.py file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            count += 1
-        except (TypeError, ValueError):
-            break
-    print()
-    return count
+
+def safe_print_integer(value):
+    """
+    Prints a value if it's an integer.
+
+    Args:
+        value: The value to be printed.
+
+    Returns:
+        True if the value is printed successfully (i.e., it's an integer);
+        otherwise, returns False.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
