15ed8d5f7e55228c66048b9192093ab5f0b13ab3
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-26 21:54:20 +0000

    Add 4-square.py file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Definition of the class 'Square' with controlled access to the private attribute 'size' 
+and a method to compute its area.
+"""
+
+
+class Square:
+    """
+    A representation of a square.
+
+    Attributes:
+        __size (int): Size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the Square with an optional size.
+
+        Args:
+            size (int): Size of the square. Default is 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter for size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter for size.
+
+        Args:
+            value (int): Size of the square.
+        """
+        # Check if value is an integer
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        # Check if value is a non-negative integer
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Return the area of the square.
+        """
+        return self.__size * self.__size
