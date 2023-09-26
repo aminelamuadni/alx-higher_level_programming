a04b0a985b8ad292012b321a3cb5b1d0455cdd6b
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-26 22:28:58 +0000

    Update 103-magic_class.py file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
+"""
+This module defines the MagicClass which calculates the area and
+circumference based on a given radius.
+"""
+
 import math
 
 
 class MagicClass:
-    """MagicClass that matches the given bytecode"""
+    """
+    Represents a MagicClass which calculates area and circumference
+    for a given radius.
+    """
 
     def __init__(self, radius=0):
-        """Initialize the MagicClass instance."""
+        """
+        Initializes the MagicClass instance.
+
+        Args:
+            radius (int or float): The radius of the MagicClass. Default is 0.
+        """
         self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
+        if not isinstance(radius, (int, float)):
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Return the area of the MagicClass."""
+        """
+        Calculates the area of the MagicClass.
+
+        Returns:
+            The area.
+        """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """Return the circumference of the MagicClass."""
+        """
+        Calculates the circumference of the MagicClass.
+
+        Returns:
+            The circumference.
+        """
         return 2 * math.pi * self.__radius
