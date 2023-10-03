e39f5f5d1cd7ba096376a619e1e941e14a6d9c6d
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-03 02:03:42 +0000

    Update 6-rectangle.py file

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
         height (int): height of the rectangle.
         number_of_instances (int): number of Rectangle instances.
     """
-    
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
