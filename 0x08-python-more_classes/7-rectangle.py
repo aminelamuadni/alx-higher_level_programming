1d7f0045c383561d8cd41c6839dbc5dce7b3b245
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-03 02:10:21 +0000

    Update 7-rectangle.py file

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -74,7 +74,10 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return the string representation of the rectangle using print_symbol."""
+        """
+        Return the string representation of the rectangle 
+        using the set print_symbol.
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         symbol = str(self.print_symbol)
