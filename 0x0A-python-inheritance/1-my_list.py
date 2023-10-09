08d86467f7d6d8e0e9d6470704f5ea7623a00143
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-09 20:05:43 +0000

    Add 1-my_list.py file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for MyList class.
+"""
+
+
+class MyList(list):
+    """
+    MyList class that inherits from list.
+    """
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order.
+        """
+        print(sorted(self))
