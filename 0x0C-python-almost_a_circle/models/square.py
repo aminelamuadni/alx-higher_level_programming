637a8f5b1327e183ef926332abe4a352cc11831a
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-18 13:24:27 +0000

    Add models/square.py file

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines the Square class, a subclass of Rectangle."""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Constructor for Square.
+
+        Attributes:
+            size (int): Size of the square.
+            x (int, optional): x position. Defaults to 0.
+            y (int, optional): y position. Defaults to 0.
+            id (int, optional): Id of the square. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        String representation of the Square.
+
+        Returns:
+            str: [Square] (<id>) <x>/<y> - <size>
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
