f0b97517c69c80e1be3cad9f159e22d543ae192a
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-18 15:36:35 +0000

    Update tests/test_models/test_base.py file

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,86 +1,135 @@
 #!/usr/bin/python3
 """
-This module contains the Base class which serves as the
-"base" for all other classes in this project. The main
-purpose of the Base class is to manage the `id` attribute
-for all derived classes.
+This module contains the unit tests for the Base class.
 """
 
 
+import os
 import json
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-class Base:
-    """Base class for the project."""
+class TestBase(unittest.TestCase):
+    """
+    Defines the test cases for the Base class.
+    """
 
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """Initialize the Base instance.
-
-        Args:
-            id (int, optional): The id of the instance. Defaults to None.
+    def setUp(self):
         """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
+        Resets the Base class private counter before each test.
         """
-        Returns the JSON string representation of list_dictionaries.
+        Base._Base__nb_objects = 0
 
-        Args:
-            list_dictionaries (list): A list of dictionaries.
+    def test_id_assignment(self):
+        """Tests the ID assignment of the Base class."""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
-        Returns:
-            str: The JSON string representation of list_dictionaries.
-        """
-        if not list_dictionaries or list_dictionaries is None:
-            return "[]"
-        return json.dumps(list_dictionaries)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
 
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """
-        Writes the JSON string representation of list_objs to a file.
-        """
-        list_dicts = []
-        if list_objs is not None:
-            list_dicts = [obj.to_dictionary() for obj in list_objs]
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
 
-        with open(cls.__name__ + ".json", 'w') as file:
-            file.write(cls.to_json_string(list_dicts))
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
 
-    @staticmethod
-    def from_json_string(json_string):
-        """
-        Returns the list of the JSON string representation `json_string`.
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
 
-        Args:
-            json_string (str): A string representing a list of dictionaries.
+    def test_id_data_type(self):
+        """Tests non-integer IDs."""
+        b = Base("string_id")
+        self.assertEqual(b.id, "string_id")
 
-        Returns:
-            list: A list of dictionaries represented by `json_string`.
-        """
-        if json_string is None or len(json_string) == 0:
-            return []
+        b = Base(1.5)
+        self.assertEqual(b.id, 1.5)
 
-        return json.loads(json_string)
+        b = Base([1, 2, 3])
+        self.assertEqual(b.id, [1, 2, 3])
 
-    @classmethod
-    def create(cls, **dictionary):
-        """
-        Returns an instance with all attributes set from the dictionary.
-        """
-        if cls.__name__ == 'Rectangle':
-            dummy = cls(1, 1)
-        elif cls.__name__ == 'Square':
-            dummy = cls(1)
-        else:
-            raise Exception("Unknown class")
+    def test_to_json_string(self):
+        """Tests the to_json_string method of the Base class."""
 
-        dummy.update(**dictionary)
-        return dummy
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+        list_dicts = [{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]
+        json_str = Base.to_json_string(list_dicts)
+        self.assertEqual(json.loads(json_str), list_dicts)
+
+        self.assertIsInstance(json_str, str)
+
+    def test_save_to_file(self):
+        """Tests the save_to_file method of the Base class."""
+
+        class MockClass(Base):
+            def to_dictionary(self):
+                return {"id": self.id}
+
+        obj1 = MockClass()
+        obj2 = MockClass()
+
+        MockClass.save_to_file([obj1, obj2])
+
+        self.assertTrue(os.path.isfile("MockClass.json"))
+
+        with open("MockClass.json", "r") as file:
+            content = json.load(file)
+            expected = [{"id": 1}, {"id": 2}]
+            self.assertEqual(content, expected)
+
+        os.remove("MockClass.json")
+
+    def test_from_json_string(self):
+        """Tests the from_json_string method of the Base class."""
+
+        self.assertEqual(Base.from_json_string('[]'), [])
+        
+        json_str = '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]'
+        self.assertEqual(Base.from_json_string(json_str), [{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}])
+
+        self.assertEqual(Base.from_json_string(None), [])
+
+        self.assertEqual(Base.from_json_string(""), [])
+
+    def test_create(self):
+        """Tests the create method of the Base class."""
+        rect_dict = {"id": 1, "width": 4, "height": 5, "x": 0, "y": 0}
+        r = Rectangle.create(**rect_dict)
+        self.assertEqual(r.width, 4)
+        self.assertEqual(r.height, 5)
+
+        square_dict = {"id": 2, "size": 6}
+        s = Square.create(**square_dict)
+        self.assertEqual(s.size, 6)
+
+    def test_load_from_file(self):
+        """Tests the load_from_file method of the Base class."""
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+
+        self.assertEqual(Rectangle.load_from_file(), [])
+
+        r1 = Rectangle(4, 5, 0, 0, 1)
+        r2 = Rectangle(6, 7, 0, 0, 2)
+        Rectangle.save_to_file([r1, r2])
+
+        rectangles = Rectangle.load_from_file()
+        self.assertEqual(len(rectangles), 2)
+        self.assertIsInstance(rectangles[0], Rectangle)
+        self.assertIsInstance(rectangles[1], Rectangle)
+        self.assertEqual(rectangles[0].width, 4)
+        self.assertEqual(rectangles[0].height, 5)
+        self.assertEqual(rectangles[1].width, 6)
+        self.assertEqual(rectangles[1].height, 7)
+
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+
+if __name__ == "__main__":
+    unittest.main()
