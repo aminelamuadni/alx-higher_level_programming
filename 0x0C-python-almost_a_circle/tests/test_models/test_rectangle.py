40fafc6a445be30bc6e26cdff51876d62d54a2ff
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-18 13:03:55 +0000

    Update tests/test_models/test_rectangle.py file

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -119,5 +119,27 @@ class TestRectangle(unittest.TestCase):
         sys.stdout = sys.__stdout__
         self.assertEqual(output, expected_output)
 
+    def test_update(self):
+        """Test the update method of the Rectangle class."""
+        r1 = Rectangle(10, 10, 10, 10)
+
+        r1.update(89)
+        self.assertEqual(r1.id, 89)
+
+        r1.update(89, 2)
+        self.assertEqual(r1.width, 2)
+
+        r1.update(89, 2, 3)
+        self.assertEqual(r1.height, 3)
+
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(r1.x, 4)
+
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(r1.y, 5)
+
+        r1.update(89, 2, 3, 4, 5, 6, 7)
+        self.assertEqual(r1.y, 5)
+
 if __name__ == "__main__":
     unittest.main()
