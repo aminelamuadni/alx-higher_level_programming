72c844b03c4c5f4eaa0e14a4ef9081bc0a8b9275
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-18 12:10:27 +0000

    Add models/base.py file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains the Base class which serves as the
+"base" for all other classes in this project. The main 
+purpose of the Base class is to manage the `id` attribute 
+for all derived classes.
+"""
+
+
+class Base:
+    """Base class for the project."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize the Base instance.
+
+        Args:
+            id (int, optional): The id of the instance. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
