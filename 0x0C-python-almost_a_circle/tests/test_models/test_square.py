87a5ffdb2bd56cbc78975225647293bcc08ff20a
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-18 13:30:52 +0000

    Update tests/test_models/test_square.py file

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -13,6 +13,28 @@ class TestSquare(unittest.TestCase):
     Defines the test cases for the Square class.
     """
 
+    def test_size_getter(self):
+        """
+        Test cases for the size getter.
+        """
+        s1 = Square(5)
+        self.assertEqual(s1.size, 5)
+        
+        s2 = Square(3, 2)
+        self.assertEqual(s2.size, 3)
+
+    def test_size_setter(self):
+        """
+        Test cases for the size setter.
+        """
+        s1 = Square(5)
+        s1.size = 10
+        self.assertEqual(s1.width, 10)
+        self.assertEqual(s1.height, 10)
+
+        with self.assertRaises(TypeError):
+            s1.size = "9"
+
     def test_square_creation(self):
         """
         Test cases for the creation of squares and
@@ -20,21 +42,15 @@ class TestSquare(unittest.TestCase):
         """
         s1 = Square(5)
         self.assertTrue(hasattr(s1, 'id'))
-        self.assertEqual(s1.width, 5)
-        self.assertEqual(s1.x, 0)
-        self.assertEqual(s1.y, 0)
+        self.assertEqual(s1.size, 5)
 
         s2 = Square(2, 2)
         self.assertTrue(hasattr(s2, 'id'))
-        self.assertEqual(s2.width, 2)
-        self.assertEqual(s2.x, 2)
-        self.assertEqual(s2.y, 0)
+        self.assertEqual(s2.size, 2)
 
         s3 = Square(3, 1, 3)
         self.assertTrue(hasattr(s3, 'id'))
-        self.assertEqual(s3.width, 3)
-        self.assertEqual(s3.x, 1)
-        self.assertEqual(s3.y, 3)
+        self.assertEqual(s3.size, 3)
 
     def test_square_area(self):
         """
