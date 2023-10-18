5acf63ba6c7b1e6186067f98dca7e27e25a185e1
Amine Lamuadni <W7equwE5@gmail.com>
2023-10-18 15:10:05 +0000

    Update models/square.py file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -62,3 +62,14 @@ class Square(Rectangle):
         for key, value in kwargs.items():
             if hasattr(self, key):
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of a Square instance.
+        """
+        return {
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+        }
