e267cb270492a054a7a4a10dede61f28065eabdc
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-15 01:35:23 +0000

    Update 100-move_to_utf8.sql file

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,6 +5,3 @@ USE hbtn_0c_0;
 
 -- Convert the entire table, affecting all character columns
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Specifically convert the 'name' field
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
