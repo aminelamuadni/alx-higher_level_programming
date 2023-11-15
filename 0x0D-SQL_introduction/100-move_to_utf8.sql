bbba3ee8b3c3938dc4fcfa46b2290381c3586792
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-15 01:21:17 +0000

    Update 100-move_to_utf8.sql file

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,10 @@
--- Script to convert 'hbtn_0c_0' database and 'first_table' to UTF8
+-- Script to convert 'hbtn_0c_0' database, 'first_table', and 'name' field to UTF8
 
--- Convert the database
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Select the database
+USE hbtn_0c_0;
 
--- Convert the table
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the entire table, affecting all character columns
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the specific field
-ALTER TABLE hbtn_0c_0.first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Specifically convert the 'name' field
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
