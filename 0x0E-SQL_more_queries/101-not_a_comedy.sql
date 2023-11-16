3a51f227998cd9d3f6de83b1788bca9d1f27d9d0
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-16 01:44:45 +0000

    Add 101-not_a_comedy.sql file

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- Script to list all shows not categorized under the genre 'Comedy'
+
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+    SELECT tv_shows.id
+    FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
