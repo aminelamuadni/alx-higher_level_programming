e6d80babf8b8386e1edb35a46f36d3bde0644395
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-16 01:37:29 +0000

    Update 13-count_shows_by_genre.sql file

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,6 +4,6 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-GROUP BY genres.name
+GROUP BY tv_genres.name
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
