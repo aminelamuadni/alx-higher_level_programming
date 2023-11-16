629e21bf67e059d68821728035680b2af1148dac
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-16 01:50:31 +0000

    Update 103-rating_genres.sql file

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- Script to list all genres by their rating sum
 
-SELECT g.name, SUM(r.rate) as rating
+SELECT g.name, SUM(r.rate) AS rating
 FROM tv_genres g
 JOIN tv_show_genres sg ON g.id = sg.genre_id
 JOIN tv_show_ratings r ON sg.show_id = r.show_id
