b4bc565a539b79572ad7f6d046e10cce697510f9
Amine Lamuadni <W7equwE5@gmail.com>
2023-11-16 01:29:59 +0000

    Update 13-count_shows_by_genre.sql file

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Script to list genres and the number of shows linked to each
 
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 GROUP BY genres.name
 HAVING COUNT(tv_show_genres.show_id) > 0
