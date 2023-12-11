69bf83f0a02a8af999baa174f173b93e9fe7e0b2
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-11 13:18:35 +0000

    Update 9-add.js file

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  const sum = a + b;
-
-  console.log(sum);
+    return a + b;
 }
 
-add(Number(process.argv[2]), Number(process.argv[3]));
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+console.log(add(num1, num2));
