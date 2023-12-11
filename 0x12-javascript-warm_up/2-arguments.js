e36672ad0fd446c940f1843d47f8c4178103b586
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-11 12:49:26 +0000

    Update 2-arguments.js file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
+if (args.argv.length === 2) {
     console.log('No argument');
-} else if (args.length === 1) {
+} else if (args.argv.length === 3) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
