d1963faeba9c436f9a71ea2b0ccbcaa0cc7f938f
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-12 20:49:51 +0000

    Update 8-esrever.js file

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let reversedList = [];
+  let list = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    reversedList.push(list[i]);
+    list.push(list[i]);
   }
-  return reversedList;
+  return list;
 };
