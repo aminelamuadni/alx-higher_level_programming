ce42b5a583c5ea1c536e01a0224dddb36cdb2de6
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-17 22:55:28 +0000

    Update 0-select_states.py file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -42,6 +42,7 @@ def list_states(username, password, db_name):
         if db:
             db.close()
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         list_states(sys.argv[1], sys.argv[2], sys.argv[3])
