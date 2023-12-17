ce701d5ecbbafef6d4c279b0762317f9b3e59ea4
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-17 22:22:58 +0000

    Update 0-select_states.py file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+This script is designed to connect to a MySQL database and list all the states
+in the database. It demonstrates basic usage of the MySQLdb module to interact
+with a MySQL database from Python.
+
+Usage:
+    ./0-select_states.py <mysql username> <mysql password> <database name>
+
+The script takes three command-line arguments:
+- MySQL username
+- MySQL password
+- Database name
+
+It connects to a MySQL server running on localhost at port 3306, queries
+the 'states' table, and prints each state's id and name in ascending order by
+id.
+"""
+
 import MySQLdb
 import sys
 
@@ -7,7 +25,9 @@ def list_states(username, password, db_name):
     Lists all states from the database.
     """
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+        db = MySQLdb.connect(host="localhost", port=3306, 
+                             user=username, passwd=password, 
+                             db=db_name, charset="utf8")
         cur = db.cursor()
         cur.execute("SELECT * FROM states ORDER BY id ASC")
         for row in cur.fetchall():
@@ -24,4 +44,5 @@ if __name__ == "__main__":
     if len(sys.argv) == 4:
         list_states(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        print("Usage: ./0-select_states.py <mysql username> <mysql password> <database name>")
+        print("Usage: ./0-select_states.py <mysql username> "
+              "<mysql password> <database name>")
