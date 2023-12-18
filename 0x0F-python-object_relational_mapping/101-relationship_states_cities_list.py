4c1c410af83ec60b0f7f93a595286328ab9a91b1
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-18 00:29:20 +0000

    Add 101-relationship_states_cities_list.py file

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects, and corresponding City objects,
+contained in the database 'hbtn_0e_101_usa'.
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+import sys
+
+
+def list_states_and_cities(username, password, db_name):
+    """
+    Lists all State objects and their corresponding City objects from the
+    database.
+    """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name
+        )
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id, State.cities).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        list_states_and_cities(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        print("Usage: ./101-relationship_states_cities_list.py "
+              "<mysql username> <mysql password> <database name>")
