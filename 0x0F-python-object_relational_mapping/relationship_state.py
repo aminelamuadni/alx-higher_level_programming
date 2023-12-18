9f2209f0cf8c18bf6cba737e579c5c7ae720739c
Amine Lamuadni <W7equwE5@gmail.com>
2023-12-18 00:26:30 +0000

    Add relationship_state.py file

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Definition of the State class with a relationship to the City class.
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class:
+    - Inherits from Base.
+    - Links to MySQL table 'states'.
+    - Contains id and name attributes.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
