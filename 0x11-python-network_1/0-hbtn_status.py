2280c614fa30a8e93a576d668de221ff13d11420
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-07 18:34:14 +0000

    Add 0-hbtn_status.py file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script to fetch and display the status from a URL using the urllib package.
+This script demonstrates basic network programming concepts like making an HTTP
+GET request and handling the response in Python.
+"""
+
+
+import urllib.request
+
+
+def fetch_status(url):
+    """
+    Fetches the status from a given URL using urllib and prints the response
+    content, its type, and the UTF-8 decoded content.
+    """
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    fetch_status(url)
