2bc628a179e6f054cea28daaac63b8a269a1e137
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-07 18:42:22 +0000

    Update 0-hbtn_status.py file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,14 +14,14 @@ def fetch_status(url):
     Fetches the status from a given URL using urllib and prints the response
     content, its type, and the UTF-8 decoded content.
     """
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen(url) as response:
         content = response.read()
 
         print("Body response:")
         print(f"\t- type: {type(content)}")
         print(f"\t- content: {content}")
         print(f"\t- utf8 content: {content.decode('utf-8')}")
+
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
