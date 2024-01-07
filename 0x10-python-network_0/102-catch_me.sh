4676bef93d91a04a0eb44945c99d9a33c85d72ca
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-07 18:29:46 +0000

    Update 102-catch_me.sh file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a specific endpoint that responds with "You got me!"
-curl -sLX PUT 0.0.0.0:5000/catch_me -H "origin: HolbertonSchool"
+curl -sLX PUT 0.0.0.0:5000/catch_me -H "origin: HolbertonSchool" -d "user_id=98"
