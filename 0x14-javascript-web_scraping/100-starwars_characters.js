979a9be8ab3370c02587ea419ceb74e606663562
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-30 15:58:44 +0000

    Add 100-starwars_characters.js file

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('error:', error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
