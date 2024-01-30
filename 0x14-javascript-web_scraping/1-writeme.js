d96fa292238fec2b3454d3d8c80f919fb032b8b4
Amine Lamuadni <W7equwE5@gmail.com>
2024-01-30 15:49:21 +0000

    Update 1-writeme.js file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, (err) => {
+fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.error(err);
     return;
