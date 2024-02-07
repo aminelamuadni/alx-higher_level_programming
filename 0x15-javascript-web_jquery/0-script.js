c764bf382111af02d55cfa358a89cf26d91da8d3
Amine Lamuadni <W7equwE5@gmail.com>
2024-02-07 23:59:36 +0100

    Update 0-script.js file

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const header = document.querySelector('header');
-  header.style.color = '#FF0000';
+$(document).ready(function () {
+  $('header').css('color', '#FF0000');
 });
